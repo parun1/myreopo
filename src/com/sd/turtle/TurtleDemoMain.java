cda169184f5a2814bbae9152652bb7728f4cba42
parun1 <gjgj74@naver.com>
2016-03-09 17:58:01 +0900

    sdfg

---
+++ src/com/sd/turtle/TurtleDemoMain.java
@@ -0,0 +1,26 @@
+/* 
+ * @author
+ * @since 20160309
+ * a simpe turtle demo
+ */
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain {
+  Turtle t1 = new Turtle();
+
+  TurtleDemoMain() {
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+  }
+
+  public static void main(String[] args) {
+    new TurtleDemoMain();
+  }
+}
